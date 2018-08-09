adaf307a7326a8e0cc36b116190a1be3f022869b
Jomar Tigcal <jomar@tigcal.com>
2018-08-08 23:24:17 -0700

    Updating transformer criteria value text when updating the corresponding seekbar (in the add transformer dialog)
    Signed-off-by: Jomar Tigcal <jomar@tigcal.com>

--- app/src/main/java/com/tigcal/aeq/transformation/AddTransformerFragment.java
+++ app/src/main/java/com/tigcal/aeq/transformation/AddTransformerFragment.java
@@ -12,8 +12,19 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class AddTransformerFragment extends DialogFragment {
+    private TextView strengthText;
+    private TextView intelligenceText;
+    private TextView speedText;
+    private TextView enduranceText;
+    private TextView rankText;
+    private TextView courageText;
+    private TextView firepowerText;
+    private TextView skillText;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -34,6 +45,76 @@ public class AddTransformerFragment extends DialogFragment {
                 Snackbar.make(view, "TODO: Add Transformer", Snackbar.LENGTH_SHORT).show();
             }
         });
+
+        strengthText = view.findViewById(R.id.transformer_strength_text);
+        intelligenceText = view.findViewById(R.id.transformer_intelligence_text);
+        speedText = view.findViewById(R.id.transformer_speed_text);
+        enduranceText = view.findViewById(R.id.transformer_endurance_text);
+        rankText = view.findViewById(R.id.transformer_rank_text);
+        courageText = view.findViewById(R.id.transformer_courage_text);
+        firepowerText = view.findViewById(R.id.transformer_firepower_text);
+        skillText = view.findViewById(R.id.transformer_skill_text);
+
+        SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                switch (seekBar.getId()) {
+                    case R.id.transformer_strength_seekbar:
+                        strengthText.setText(String.valueOf(progress));
+                        break;
+                    case R.id.transformer_intelligence_seekbar:
+                        intelligenceText.setText(String.valueOf(progress));
+                        break;
+                    case R.id.transformer_speed_seekbar:
+                        speedText.setText(String.valueOf(progress));
+                        break;
+                    case R.id.transformer_endurance_seekbar:
+                        enduranceText.setText(String.valueOf(progress));
+                        break;
+                    case R.id.transformer_rank_seekbar:
+                        rankText.setText(String.valueOf(progress));
+                        break;
+                    case R.id.transformer_courage_seekbar:
+                        courageText.setText(String.valueOf(progress));
+                        break;
+                    case R.id.transformer_firepower_seekbar:
+                        firepowerText.setText(String.valueOf(progress));
+                        break;
+                    case R.id.transformer_skill_seekbar:
+                        skillText.setText(String.valueOf(progress));
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        };
+
+        SeekBar strengthSeekbar = view.findViewById(R.id.transformer_strength_seekbar);
+        strengthSeekbar.setOnSeekBarChangeListener(seekBarChangeListener);
+        SeekBar intelligenceSeekbar = view.findViewById(R.id.transformer_intelligence_seekbar);
+        intelligenceSeekbar.setOnSeekBarChangeListener(seekBarChangeListener);
+        SeekBar speedSeekbar =  view.findViewById(R.id.transformer_speed_seekbar);
+        speedSeekbar.setOnSeekBarChangeListener(seekBarChangeListener);
+        SeekBar enduranceSeekbar =  view.findViewById(R.id.transformer_endurance_seekbar);
+        enduranceSeekbar.setOnSeekBarChangeListener(seekBarChangeListener);
+        SeekBar rankSeekbar =  view.findViewById(R.id.transformer_rank_seekbar);
+        rankSeekbar.setOnSeekBarChangeListener(seekBarChangeListener);
+        SeekBar courageSeekbar =  view.findViewById(R.id.transformer_courage_seekbar);
+        courageSeekbar.setOnSeekBarChangeListener(seekBarChangeListener);
+        SeekBar firepowerSeekbar =  view.findViewById(R.id.transformer_firepower_seekbar);
+        firepowerSeekbar.setOnSeekBarChangeListener(seekBarChangeListener);
+        SeekBar skillSeekbar =  view.findViewById(R.id.transformer_skill_seekbar);
+        skillSeekbar.setOnSeekBarChangeListener(seekBarChangeListener);
 
         return view;
     }
