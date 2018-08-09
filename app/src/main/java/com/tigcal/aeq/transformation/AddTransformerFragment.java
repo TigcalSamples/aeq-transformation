31b17eedd279e09dd1a4832a26d9518b47c2b13e
Jomar Tigcal <jomar@tigcal.com>
2018-08-08 22:53:44 -0700

    Adding close and add click listeners in the add transformer dialog
    Signed-off-by: Jomar Tigcal <jomar@tigcal.com>

--- app/src/main/java/com/tigcal/aeq/transformation/AddTransformerFragment.java
+++ app/src/main/java/com/tigcal/aeq/transformation/AddTransformerFragment.java
@@ -4,20 +4,39 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class AddTransformerFragment extends DialogFragment {
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.dialog_add_transformer, container, false);
-    }
+        View view = inflater.inflate(R.layout.dialog_add_transformer, container, false);
 
+        ImageButton closeButton = view.findViewById(R.id.close_button);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+
+        Button addButton = view.findViewById(R.id.add_transformer_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "TODO: Add Transformer", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
 
     @NonNull
     @Override
